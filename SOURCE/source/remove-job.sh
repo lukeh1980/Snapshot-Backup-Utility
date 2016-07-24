59301c5675fdc673a080f029dc4d6712987dc930
Luke Higgs <lukeh1980@users.noreply.github.com>
2016-07-23 20:23:12 -0400

    Add files via upload

---
+++ SOURCE/source/remove-job.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+### --------------------------------- ###
+###     Copyright 2016 Luke Higgs     ###
+### Contact: admin@aquariandesign.com ###
+### --------------------------------- ###
+
+# This file is part of SBU (Snapshot Backup Utility)
+
+# SBU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# SBU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with SBU (located in /opt/sbu/docs/COPYING).  If not, see <http://www.gnu.org/licenses/>.
+#################################################################################################
+
+source /opt/sbu/source/functions
+source /opt/sbu/source/header
+
+DELFILES=$2
+
+if [[ $(checkStatus $NAME) -gt 0 ]]; then
+	/opt/sbu/sbu --stop $NAME
+fi
+
+echo ""
+echo "-------REMOVING $NAME BACKUP JOB-------"
+echo ""
+
+if [ -s "/opt/sbu/source/autostart.sh" ]; then
+	grep -v "/opt/sbu/sbu --start $NAME" /opt/sbu/source/autostart.sh > /opt/sbu/source/autostart.tmp; mv /opt/sbu/source/autostart.tmp /opt/sbu/source/autostart.sh 
+	chmod a+x /opt/sbu/source/*
+fi
+
+if [ ! -d "/var/log/sbu/$NAME" ]; then
+	echo "Log directory /var/log/sbu/$NAME does not exist..."
+	exit
+else
+	rm -rf /var/log/sbu/$NAME
+fi
+
+if [ ! -d "/opt/sbu/jobs/$NAME" ]; then
+	echo "Job directory /opt/sbu/jobs/"$NAME" does not exist..."
+	exit
+else
+	rm -rf /opt/sbu/jobs/$NAME
+fi
+
+if [[ $DELFILES -eq 1 ]]; then
+
+	if [ ! -d "${DEST}/$NAME" ]; then
+		echo "Job snapshots not found..."
+		exit
+	else
+		
+		mv "${DEST}/$NAME" "${DEST}/.$NAME.deleting"
+		
+		# create delete queue file:
+		echo "#!/bin/bash" > /opt/sbu/delqueue/$NAME-delfiles.sh
+		echo "if [ ! -d \""${DEST}/.$NAME.deleting"\" ]; then" >> /opt/sbu/delqueue/$NAME-delfiles.sh
+		echo "rm -rf /opt/sbu/delqueue/$NAME-delfiles.sh" >> /opt/sbu/delqueue/$NAME-delfiles.sh
+		echo "else" >> /opt/sbu/delqueue/$NAME-delfiles.sh
+		echo "nohup rm -rf ${DEST}/.$NAME.deleting &>/dev/null &" >> /opt/sbu/delqueue/$NAME-delfiles.sh
+		echo "fi" >> /opt/sbu/delqueue/$NAME-delfiles.sh
+		
+		chmod a+x /opt/sbu/delqueue/*
+		
+		# fire off delete command:
+		nohup rm -rf "${DEST}/.$NAME.deleting" &>/dev/null &
+		
+	fi
+	
+fi
+
+echo "$NAME backup job removed!"
---
+++ SOURCE/source/rotate-bu.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+### --------------------------------- ###
+###     Copyright 2016 Luke Higgs     ###
+### Contact: admin@aquariandesign.com ###
+### --------------------------------- ###
+
+# This file is part of SBU (Snapshot Backup Utility)
+
+# SBU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# SBU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with SBU (located in /opt/sbu/docs/COPYING).  If not, see <http://www.gnu.org/licenses/>.
+#################################################################################################
+
+source /opt/sbu/source/header
+
+clear
+echo "-------------Starting Rotate-BU Script-------------"
+echo ""
+
+if [ -s "${DEST}/$NAME/tmp/$INTERVAL-min" ]; then
+
+	INCREMENT=()
+	DIR="${DEST}/$NAME/snapshots/"
+
+	echo "Changing directory to "$DIR
+	cd "${DIR}"
+	echo ""
+	echo "Getting list of rotated snapshots..."
+	echo $(date "+%Y-%m-%d %H:%M:%S")" - Rotating Snapshots" >> /var/log/sbu/$NAME/sbulog
+	ls -v | grep -v '\.full$' | grep -v 'full-backup.index' | grep -v 'timestamp-full-backup' > "${DEST}/$NAME/tmp/$NAME-dir-list"
+	
+	INCREMENT=$(tail -n 1 "${DEST}/$NAME/tmp/$NAME-dir-list" | cut -d '.' -f 2)
+
+	while [ $INCREMENT -gt -1 ]; do
+		
+		if [ $INCREMENT -gt 0 ]; then
+			#echo $(date "+%Y-%m-%d %H:%M:%S")" - Moving $DIR$NAME.$INCREMENT to $DIR$NAME.$((INCREMENT+1))" >> /var/log/sbu/$NAME/sbulog
+			mv "${DIR}$NAME.$INCREMENT" "${DIR}$NAME.$((INCREMENT+1))"
+			let "INCREMENT--"
+		else
+			#echo $(date "+%Y-%m-%d %H:%M:%S")" - Moving $DIR$NAME.$INCREMENT to $DIR$NAME.$((INCREMENT+1))" >> /var/log/sbu/$NAME/sbulog
+			mv "${DIR}$NAME.$INCREMENT" "${DIR}$NAME.$((INCREMENT+1))"
+			#echo $(date "+%Y-%m-%d %H:%M:%S")" - Moving $DEST/$NAME/tmp/.$NAME.snapshot to $DIR$NAME.$INCREMENT" >> /var/log/sbu/$NAME/sbulog
+			mv "${DEST}/$NAME/tmp/.$NAME.snapshot" "${DIR}$NAME.$INCREMENT"
+			let "INCREMENT--"
+		fi
+		
+	done
+
+	rm -rf "${DEST}/$NAME/tmp/$NAME-dir-list"
+
+	echo ""
+	echo "-------------END Rotate-BU Script-------------"
+	echo ""
+
+fi
